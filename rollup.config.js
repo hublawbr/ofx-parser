3487e285bc61dc7bcaa2b8e081eadcebb02d8632
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2020-03-10 14:53:56 -0600

    adding peer dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -21,9 +21,7 @@ export default {
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-    'url',
-    'tls'
+    ...Object.keys(pkg.peerDependencies || {})
   ],
   plugins: [
     resolve({
